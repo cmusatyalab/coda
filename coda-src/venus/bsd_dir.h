ecfb8c1e8a89c8c6e7b8ce325bd28c35b297bf63
Peter Braam <braam@cs.cmu.edu>
1996-11-22 20:50:30 +0000

    include venus dirent.h

--- coda-src/venus/bsd_dir.h
+++ coda-src/venus/bsd_dir.h
@@ -29,7 +29,7 @@ improvements or extensions that  they  make,  and  to  grant  Carnegie
 Mellon the rights to redistribute these changes without encumbrance.
 */
 
-static char *rcsid = "$Header: blurb.doc,v 1.1 96/11/22 13:29:31 raiff Exp $";
+static char *rcsid = "$Header: /afs/cs.cmu.edu/project/coda-braam/peter-objs/coda-src/venus/RCS/bsd-dir.h,v 1.1 1996/11/22 19:11:41 braam Exp braam $";
 #endif /*_BLURB_*/
 
 /*	$NetBSD: dir.h,v 1.8 1994/06/29 06:43:52 cgd Exp $	*/
@@ -77,7 +77,7 @@ static char *rcsid = "$Header: blurb.doc,v 1.1 96/11/22 13:29:31 raiff Exp $";
 #ifndef _SYS_DIR_H_
 #define	_SYS_DIR_H_
 
-#include <dirent.h>
+#include <venus-dirent.h>
 
 /*
  * Backwards compatibility.
