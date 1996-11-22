b9d461010f8ccb4ed30b756ab2dbd58d998cd9bd
Peter Braam <braam@cs.cmu.edu>
1996-11-22 21:32:22 +0000

    added include of dirent.h for Linux.

--- coda-src/vol/volume.cc
+++ coda-src/vol/volume.cc
@@ -29,7 +29,7 @@ improvements or extensions that  they  make,  and  to  grant  Carnegie
 Mellon the rights to redistribute these changes without encumbrance.
 */
 
-static char *rcsid = "$Header: blurb.doc,v 1.1 96/11/22 13:29:31 raiff Exp $";
+static char *rcsid = "$Header: /afs/cs.cmu.edu/project/coda-braam/peter-objs/coda-src/vol/RCS/volume.cc,v 1.1 1996/11/22 19:10:35 braam Exp braam $";
 #endif /*_BLURB_*/
 
 
@@ -83,6 +83,9 @@ extern "C" {
 #include <unistd.h>
 #include <stdlib.h>
 #endif __NetBSD__
+#ifdef LINUX
+#include <dirent.h>
+#endif
 
 #include <lwp.h>
 #include <lock.h>
