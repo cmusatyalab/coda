bc8eed86b193aa6c0813a6dace872fa9967c71e4
Lily Mummert <lily@cs.cmu.edu>
1997-01-29 18:13:42 +0000

    added include of inodefs.h

--- coda-src/rpc2/sftp3.c
+++ coda-src/rpc2/sftp3.c
@@ -29,7 +29,7 @@ improvements or extensions that  they  make,  and  to  grant  Carnegie
 Mellon the rights to redistribute these changes without encumbrance.
 */
 
-static char *rcsid = "$Header: /afs/cs.cmu.edu/project/coda-braam/src/coda-4.0.1/RCSLINK/./coda-src/rpc2/sftp3.c,v 1.1 1996/11/22 19:07:37 braam Exp $";
+static char *rcsid = "$Header: sftp3.c,v 4.1 97/01/08 21:50:31 rvb Exp $";
 #endif /*_BLURB_*/
 
 
@@ -75,7 +75,7 @@ supported by Transarc Corporation, Pittsburgh, PA.
 #include "rpc2.private.h"
 #include "se.h"
 #include "sftp.h"
-
+#include <inodefs.h>
 
 extern int errno;
 
