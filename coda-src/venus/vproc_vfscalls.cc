f52acf6ee37d64980ff2ac3ef13b11fddb459da2
Peter Braam <braam@cs.cmu.edu>
1997-04-24 09:00:42 +0000

    small change made to filling in vattr's for inconsistent objects under Linux.

--- coda-src/venus/vproc_vfscalls.cc
+++ coda-src/venus/vproc_vfscalls.cc
@@ -29,7 +29,7 @@ improvements or extensions that  they  make,  and  to  grant  Carnegie
 Mellon the rights to redistribute these changes without encumbrance.
 */
 
-static char *rcsid = "$Header: /coda/usr/lily/src/coda-src/venus/RCS/vproc_vfscalls.cc,v 4.4 97/02/26 16:03:41 rvb Exp $";
+static char *rcsid = "$Header: /afs/cs.cmu.edu/project/coda-braam/ss/coda-src/venus/RCS/vproc_vfscalls.cc,v 4.5 1997/04/23 17:46:23 lily Exp braam $";
 #endif /*_BLURB_*/
 
 
@@ -472,7 +472,12 @@ FreeLocks:
 
 	/* Make a "fake" vattr block for the inconsistent object. */
 	va_init(vap);
+#ifndef __linux__
 	vap->va_mode = 0444 | FTTOVT(SymbolicLink);
+#else
+	vap->va_mode = 0444;
+	vap->va_type = FTTOVT(SymbolicLink);
+#endif
 	vap->va_uid = (short)V_UID;
 	vap->va_gid = (short)V_GID;
 	vap->va_fsid = 1;
