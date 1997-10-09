f66c307f5b7116eee657e01806ec857594d95ee1
Peter Braam <braam@cs.cmu.edu>
1997-10-09 21:27:07 +0000

    attempt to fix compile problem.

--- coda-src/fail/fcon.c
+++ coda-src/fail/fcon.c
@@ -29,7 +29,7 @@ improvements or extensions that  they  make,  and  to  grant  Carnegie
 Mellon the rights to redistribute these changes without encumbrance.
 */
 
-static char *rcsid = "$Header: /afs/cs.cmu.edu/project/coda-braam/src/coda-4.0.1/RCSLINK/./coda-src/fail/fcon.c,v 1.1 1996/11/22 19:09:18 braam Exp $";
+static char *rcsid = "$Header: /afs/cs/project/coda-src/cvs/coda/coda-src/fail/fcon.c,v 4.1 1997/01/08 21:49:37 rvb Exp $";
 #endif /*_BLURB_*/
 
 
@@ -48,7 +48,7 @@ static char *rcsid = "$Header: /afs/cs.cmu.edu/project/coda-braam/src/coda-4.0.1
   RPC2 interface
 
  */
-
+#include <sys/types.h>
 #include <stdio.h>
 #include <assert.h>
 #include <lwp.h>
