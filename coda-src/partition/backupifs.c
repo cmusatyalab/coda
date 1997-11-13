5b6df46601875bb48b2406bf48b8fef055664d46
Peter Braam <braam@cs.cmu.edu>
1997-11-13 18:14:14 +0000

    Partitions in use by the Coda backup program are now a supported type.
    Some further improvements to this type of partition may be useful.

---
+++ coda-src/partition/backupifs.c
@@ -0,0 +1,75 @@
+#ifndef _BLURB_
+#define _BLURB_
+/*
+
+            Coda: an Experimental Distributed File System
+                             Release 4.0
+
+          Copyright (c) 1987-1996 Carnegie Mellon University
+                         All Rights Reserved
+
+Permission  to  use, copy, modify and distribute this software and its
+documentation is hereby granted,  provided  that  both  the  copyright
+notice  and  this  permission  notice  appear  in  all  copies  of the
+software, derivative works or  modified  versions,  and  any  portions
+thereof, and that both notices appear in supporting documentation, and
+that credit is given to Carnegie Mellon University  in  all  documents
+and publicity pertaining to direct or indirect use of this code or its
+derivatives.
+
+CODA IS AN EXPERIMENTAL SOFTWARE SYSTEM AND IS  KNOWN  TO  HAVE  BUGS,
+SOME  OF  WHICH MAY HAVE SERIOUS CONSEQUENCES.  CARNEGIE MELLON ALLOWS
+FREE USE OF THIS SOFTWARE IN ITS "AS IS" CONDITION.   CARNEGIE  MELLON
+DISCLAIMS  ANY  LIABILITY  OF  ANY  KIND  FOR  ANY  DAMAGES WHATSOEVER
+RESULTING DIRECTLY OR INDIRECTLY FROM THE USE OF THIS SOFTWARE  OR  OF
+ANY DERIVATIVE WORK.
+
+Carnegie  Mellon  encourages  users  of  this  software  to return any
+improvements or extensions that  they  make,  and  to  grant  Carnegie
+Mellon the rights to redistribute these changes without encumbrance.
+*/
+#endif /*_BLURB_*/
+
+/*
+ * a dummy partition type for use by the backup system
+ * no methods are needed at all, but some can be added
+ * should we want that.
+ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif __cplusplus
+
+#if defined(__linux__) || defined(__BSD44__)
+#include <sys/types.h>
+#include <sys/param.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/dir.h>
+#include <errno.h>
+#include <ctype.h>
+#ifdef __cplusplus
+}
+#endif __cplusplus
+
+#include <util.h>
+#include "partition.h" /* this includes simpleifs.h */
+
+struct inodeops inodeops_backup = {
+    NULL, /*icreate*/
+    NULL, /*iopen*/
+    NULL, /*iread*/
+    NULL, /*iwrite*/
+    NULL, /*iinc*/
+    NULL, /*idec*/
+    NULL, /*get_header*/
+    NULL, /*put_header*/
+    NULL, /*init*/
+    NULL, /*magic*/
+    NULL /*list_coda_inodes */
+};
+
+
+#endif
