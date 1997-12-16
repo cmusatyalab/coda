3cfbf4533f8331e1b4f4f6ea5dcd45c3d7ec5918
Peter Braam <braam@cs.cmu.edu>
1997-12-16 15:31:09 +0000

    whooops

---
+++ coda-src/advice/tests/demo_handler.h
@@ -0,0 +1,8 @@
+
+#define CODADEMO "/usr/mre/russianSave/coda-src/guitools/CodaDemo.tcl"
+#define CODACONSOLEOUT "/usr/local/bin/tixwish"
+
+extern void InitializeCodaDemo();
+extern void SendToDemo(char *);
+extern char *ReadFromDemo();
+extern void ProcessInputFromDemo();
