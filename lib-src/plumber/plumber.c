29502428c229ae79d38b9261dc33fd48080b57ea
Peter Braam <braam@cs.cmu.edu>
1997-12-23 18:23:28 +0000

    Farewell plumber!!

--- lib-src/plumber/plumber.c
+++
@@ -1,7 +0,0 @@
-/* ********************************************************************** *\
- *         Copyright IBM Corporation 1988,1991 - All Rights Reserved      *
- *        For full copyright information see:'andrew/config/COPYRITE'     *
-\* ********************************************************************** */
-static char *plumber_rcsid = "$Header: /afs/cs.cmu.edu/project/coda-braam/src/coda-4.0.1/RCSLINK/./lib-src/plumber/plumber.c,v 1.1 1996/11/22 19:20:01 braam Exp $";
-
-#include "plumber.ci"
--- lib-src/plumber/pmalloc.c
+++
@@ -1,8 +0,0 @@
-/* ********************************************************************** *\
- *         Copyright IBM Corporation 1988,1991 - All Rights Reserved      *
- *        For full copyright information see:'andrew/config/COPYRITE'     *
-\* ********************************************************************** */
-static char *pmalloc_rcsid = "$Header: /afs/cs.cmu.edu/project/coda-braam/src/coda-4.0.1/RCSLINK/./lib-src/plumber/pmalloc.c,v 1.1 1996/11/22 19:19:54 braam Exp $";
-
-#define IDENTIFY
-#include "malloc.ci"
