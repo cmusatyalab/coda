510bfa3d56f2287d76f31b1cb01f2a4e6b8a2027
Peter Braam <braam@cs.cmu.edu>
1998-01-04 14:31:19 +0000

    small bug fix.

--- coda-src/util/parser.c
+++ coda-src/util/parser.c
@@ -29,7 +29,7 @@ improvements or extensions that  they  make,  and  to  grant  Carnegie
 Mellon the rights to redistribute these changes without encumbrance.
 */
 
-static char *rcsid = "$Header: /afs/cs/project/coda-src/cvs/coda/coda-src/norton/parser.cc,v 4.2 1997/04/23 13:43:52 rvb Exp $";
+static char *rcsid = "$Header: /afs/cs/project/coda-src/cvs/coda/coda-src/util/parser.c,v 4.1 1997/12/23 17:19:59 braam Exp $";
 #endif /*_BLURB_*/
 
 
@@ -161,7 +161,7 @@ static int process(char *s, char ** next, command_t *lookup,
     *prev = s; 
 
     /* non existent */
-    if ( !result ) 
+    if ( ! *result ) 
 	return CMD_NONE;
 
     /* found entry: is it ambigous, i.e. not exact command name and
