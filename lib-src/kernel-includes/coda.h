e9275819d87644b8185017d524d5cb9c6589f3c3
Bob Baron <rvb@cs.cmu.edu>
1998-01-22 22:02:07 +0000

    sigh ... __BSD44_ is not defined in the kernel

--- lib-src/kernel-includes/coda.h
+++ lib-src/kernel-includes/coda.h
@@ -132,7 +132,7 @@ typedef u_long vgid_t;
 #define _CODACRED_T_
 struct coda_cred {
     vuid_t cr_uid, cr_euid, cr_suid, cr_fsuid; /* Real, efftve, set, fs uid*/
-#ifdef	__BSD44__
+#if	defined(__NetBSD__) || defined(__FreeBSD__)
     vgid_t cr_groupid, cr_egid, cr_sgid, cr_fsgid; /* same for groups */
 #else
     vgid_t cr_gid,     cr_egid, cr_sgid, cr_fsgid; /* same for groups */
