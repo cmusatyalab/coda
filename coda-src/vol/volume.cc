f4203272fc7df5341faa8d97aee18c20bf1d7806
Henry Pierce <hmpierce@cs.cmu.edu>
1998-05-04 18:07:07 +0000

    Removed #include <linux/ext2_fs.h>

--- coda-src/vol/volume.cc
+++ coda-src/vol/volume.cc
@@ -29,7 +29,7 @@ improvements or extensions that  they  make,  and  to  grant  Carnegie
 Mellon the rights to redistribute these changes without encumbrance.
 */
 
-static char *rcsid = "$Header: /afs/cs/project/coda-src/cvs/coda/coda-src/vol/volume.cc,v 4.7 1997/11/14 13:19:28 braam Exp $";
+static char *rcsid = "$Header: /afs/cs/project/coda-src/cvs/coda/coda-src/vol/volume.cc,v 4.8 1998/01/10 18:39:45 braam Exp $";
 #endif /*_BLURB_*/
 
 
@@ -90,7 +90,6 @@ extern "C" {
 #include <stdio.h>
 #include <mntent.h>
 #include <sys/vfs.h>
-#include <linux/ext2_fs.h>
 #endif
 
 #include <lwp.h>
