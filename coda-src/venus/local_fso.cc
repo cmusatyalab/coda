b2e306b6632ac72a0a461ae6a5885ae0fb3c7f3f
Robert Watson <rnw@cs.cmu.edu>
1998-09-27 13:01:20 +0000

    Fix build on 3.0-CURRENT -- required include file.

--- coda-src/venus/local_fso.cc
+++ coda-src/venus/local_fso.cc
@@ -29,7 +29,7 @@ improvements or extensions that  they  make,  and  to  grant  Carnegie
 Mellon the rights to redistribute these changes without encumbrance.
 */
 
-static char *rcsid = "$Header: /afs/cs/project/coda-src/cvs/coda/coda-src/venus/local_fso.cc,v 4.3 1998/04/14 21:03:06 braam Exp $";
+static char *rcsid = "$Header: /afs/cs/project/coda-src/cvs/coda/coda-src/venus/local_fso.cc,v 4.4 98/08/26 21:24:33 braam Exp $";
 #endif /*_BLURB_*/
 
 
@@ -41,6 +41,7 @@ extern "C" {
 #endif __cplusplus
 #include <stdio.h>
 #include <struct.h>
+#include <sys/types.h>
 #include <netinet/in.h>
 #include <codadir.h>
 
