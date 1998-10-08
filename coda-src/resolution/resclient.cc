f3af8ab6281ccb2954a6eae1af2b1d79d7335372
Bob Baron <rvb@cs.cmu.edu>
1998-10-08 17:25:28 +0000

    Make it compile on FreeBSD -current

--- coda-src/resolution/resclient.cc
+++ coda-src/resolution/resclient.cc
@@ -29,7 +29,7 @@ improvements or extensions that  they  make,  and  to  grant  Carnegie
 Mellon the rights to redistribute these changes without encumbrance.
 */
 
-static char *rcsid = "$Header: /afs/cs/project/coda-src/cvs/coda/coda-src/res/resclient.cc,v 4.5 1998/01/10 18:37:46 braam Exp $";
+static char *rcsid = "$Header: /afs/cs/project/coda-src/cvs/coda/coda-src/res/resclient.cc,v 4.6 1998/08/31 12:23:18 braam Exp $";
 #endif /*_BLURB_*/
 
 
@@ -52,7 +52,7 @@ extern "C" {
 
 #include <struct.h>
 #ifndef __CYGWIN32__
-#include <sys/dir.h>
+#include <dirent.h>
 #endif
 
 #include <lwp.h>
