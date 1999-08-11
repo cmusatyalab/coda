f8ad8632727af11ce457ebe034d4eeed3694786d
Phil Nelson <phil@cs.wwu.edu>
1999-08-11 21:58:38 +0000

    Solaris changes:  Move some libs (eg. -ltermcap) off depends line. Add sun to if defined() to get vm_address_t and friends defined.

--- coda-src/norton/commands.cc
+++ coda-src/norton/commands.cc
@@ -149,7 +149,7 @@ long address_ok(vm_address_t addr, vm_size_t sz, vm_prot_t perm)
 }
 #endif
 
-#if    defined	(__linux__) || defined(__CYGWIN32__)
+#if    defined	(__linux__) || defined(__CYGWIN32__) || defined(sun)
 #include <sys/mman.h>
 #define vm_address_t caddr_t
 #define vm_size_t    size_t
