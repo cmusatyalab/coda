b835cf6a753a0a93a081515d3b9fa70713a118df
Phil Nelson <phil@cs.wwu.edu>
1999-08-11 21:44:04 +0000

    Provice special access to the BSD flock under sun without having to include from /usr/ucbinclude.

---
+++ lib-src/special-includes/sunflock.h
@@ -0,0 +1,28 @@
+/* BLURB lgpl
+
+                           Coda File System
+                              Release 5
+
+          Copyright (c) 1987-1999 Carnegie Mellon University
+                  Additional copyrights listed below
+
+This  code  is  distributed "AS IS" without warranty of any kind under
+the  terms of the  GNU  Library General Public Licence  Version 2,  as
+shown in the file LICENSE. The technical and financial contributors to
+Coda are listed in the file CREDITS.
+
+                        Additional copyrights
+                           none currently
+
+*/
+
+#ifndef SUNFLOCK_H
+#define SUNFLOCK_H
+
+#define LOCK_SH         0x01            /* shared file lock */
+#define LOCK_EX         0x02            /* exclusive file lock */
+#define LOCK_NB         0x04            /* don't block when locking */
+#define LOCK_UN         0x08            /* unlock file */
+int flock (int, int);
+
+#endif
