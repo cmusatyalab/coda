25785a74181e9e7caab1df764a27adcfdc8e137f
Phil Nelson <phil@cs.wwu.edu>
2000-01-13 19:18:36 +0000

    #ifdef a field out that Solaris doesn't have.

--- lib-src/lwp/src-pt/iomgr_pt.c
+++ lib-src/lwp/src-pt/iomgr_pt.c
@@ -33,7 +33,9 @@ static void iomgr_sigio_handler(int n)
     action.sa_handler  = iomgr_sigio_handler;
     sigemptyset(&action.sa_mask);
     action.sa_flags    = 0;
+#if !defined(sun) 
     action.sa_restorer = NULL;
+#endif
 
     sigaction(SIGIO, &action, NULL);
 }
