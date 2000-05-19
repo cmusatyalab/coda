e9c87f607e2095803d294c5a11034d3c76d50337
Jan Harkes <jaharkes@cs.cmu.edu>
2000-05-19 20:30:06 +0000

    Not creating the unused AllVolumes BackVols RWVols and partition files in volutil makevldb anymore.

--- coda-src/volutil/vol-makevldb.cc
+++ coda-src/volutil/vol-makevldb.cc
@@ -77,18 +77,14 @@ extern "C" {
 /* ReadWrite volumes:   are stored exactly twice in the data base.
 			Once under name of volume, once under number.
 			Only one server site will be recorded.
-			Latest read-only and backup-volumes corresponding to
-			the read-write volume will be recorded in the read
-			write entry.
+			Latest backup-volume corresponding to the read-write
+			volume will be recorded in the read write entry.
 
    Backup volumes:	Only the latest backup volume is stored by name
    			in the data base; it is also stored by number as are
 			any old backup volumes which may still exist or appear
 			to exist.  The corresponding read-write and read-only
 			volumes will not be recorded.
-
-   Readonly volumes:	Same as backup volumes.
-			
  */
 
 /* static int debug = 0; */
@@ -102,17 +98,9 @@ static int vldbSize;	  /* array size, in elements */
 static int vldbHashSize; /* Hash index space (1 to vldbHashSize) */
 static int haveEntry = 0;
 static int MaxStride;
+static FILE *volumelist;
 
 #define vldbindex(p)	((p) - &vldb_array[0])
-
-#define RWLIST_PATH	vice_file("vol/RWlist")
-#define PART_TEMP     vice_file("vol/partitions.new")
-#define PART_PATH     vice_file("vol/partitions")
-
-#define ALL_TEMP	vice_file("vol/AllVolumes.new")
-#define ALL_PATH	vice_file("vol/AllVolumes")
-
-static FILE *BackupList, *RWList, *PartList, *AllList, *volumelist;
 
 static int Pass(char type);
 static void InitAddEntry();
@@ -137,14 +125,9 @@ static void Replace(struct vldb *old, struct vldb *vnew, long date);
 static void AddServer(struct vldb *old, struct vldb *vnew);
 static void AddAssociate(struct vldb *old, struct vldb *vnew);
 static void CheckRWindex(unsigned long volume, char *name);
-static void GetPartitionInfo(char **argp, int nargs);
 static void GetArgs(char *line, char **args, int *nargs);
 static char *AllocString(char *s);
 static void GetServerNames();
-
-/* debugging hack: */
-char rootname[32];
-
 
 /*
 S_VolMakeVLDB: Rebuild the VLDB from the file listed as parameter
@@ -157,7 +140,6 @@ long S_VolMakeVLDB(RPC2_Handle rpcid, RPC2_String formal_infile)
     int MaxRO, MaxBK, MaxRW;
     int err = 0;
     ProgramType *pt;
-    char cmd[1024];
 
     /* To keep C++ 2.0 happy */
     char *infile = (char *)formal_infile;
@@ -173,44 +155,15 @@ long S_VolMakeVLDB(RPC2_Handle rpcid, RPC2_String formal_infile)
 	LogMsg(0, VolDebugLevel, stdout, "S_VolMakeVLDB: unable to open file %s", infile);
 	return(VNOVNODE);
     }
-    PartList = fopen(PART_TEMP, "w");
-    nentries = 2*Pass('P');	/* Count lines & get partition info;
-    				   have roughly twice as many data base
-				   entries as lines
-				   since each is entered under two keys */
+    nentries = 2*Pass('P');	/* Count lines; have roughly twice as many data
+				   base entries as lines since each is entered
+				   under two keys */
     vldbHashSize = nentries+(nentries/2);
     /* allow for overflow of high hash values */
     vldbSize = vldbHashSize + 100;
     vldb_array = (struct vldb *) calloc(vldbSize,  sizeof(struct vldb));
     Dates = (long *) calloc(vldbSize, sizeof (*Dates));
     RWindex = (struct vldb **) calloc(vldbSize, sizeof(*RWindex));
-
-    BackupList = fopen(BACKUPLIST_PATH, "w");
-    if (BackupList == NULL) {
-	printf("makevldb:  Unable to create %s; aborted\n", BACKUPLIST_PATH);
-	return(VNOVNODE);
-    }
-
-    RWList = fopen(RWLIST_PATH, "w");
-    if (RWList == NULL) {
-	printf("makevldb:  Unable to create %s; aborted\n", RWLIST_PATH);
-	return(VNOVNODE);
-    }
-
-#ifdef __CYGWIN32__
-    AllList = fopen(ALL_TEMP, "w");
-    if (AllList == NULL) {
-	printf("makevldb:  Unable to create %s; aborted\n", ALL_TEMP);
-  	return(VFAIL);
-    }
-#else
-    snprintf(cmd, sizeof(cmd), "sort > %s", ALL_TEMP);
-    AllList = (FILE *) popen(cmd, "w");
-    if (AllList == NULL) {
-	printf("makevldb:  Unable to \"run %s\"; aborted\n", cmd);
-  	return(VFAIL);
-    }
-#endif
 
     Pass('W');	/* Read-write volumes */
     MaxRW = MaxStride;
@@ -223,8 +176,6 @@ long S_VolMakeVLDB(RPC2_Handle rpcid, RPC2_String formal_infile)
     head->hashSize = htonl(vldbHashSize);
     
     fclose(volumelist);
-    fclose(BackupList);
-    fclose(RWList);
 
     if (haveEntry == 0) {
 	printf("makevldb:  no data base entries found in input file; aborted\n");
@@ -248,25 +199,6 @@ long S_VolMakeVLDB(RPC2_Handle rpcid, RPC2_String formal_infile)
     else
         printf("VLDB created.  Search lengths: RO %d, RW %d, BK %d.\n", MaxRO, MaxRW, MaxBK);
 
-    fclose(PartList);
-    if (rename(PART_TEMP, PART_PATH) == -1) {
-	printf("Unable to rename %s to %s; new partition list not created\n", PART_TEMP, PART_PATH);
-	err = 1;
-    }
-
-    fprintf(AllList, "# [Last line of unsorted volume list]\n");
-#ifdef __CYGWIN32__
-    fclose(AllList);
-#else
-    pclose(AllList);
-#endif
-    if (rename(ALL_TEMP, ALL_PATH) == -1) {
-	LogMsg(0, VolDebugLevel, stdout, "Unable to rename %s to %s; new volume list not created", ALL_TEMP, ALL_PATH);
-	err = 1;
-    }
-    else
-	LogMsg(0, VolDebugLevel, stdout, "%s written", ALL_PATH);
-
     /* tell fileserver to read in new database */
     VCheckVLDB();
 
@@ -278,90 +210,62 @@ static int Pass(char type)
     char *name;
     unsigned long volume;
     int server;
-    char *partition = "";
-    int minquota;
-    int maxquota;
-    int diskusage;
     unsigned long readwrite;
     int copydate, backupdate, creationdate;
-    int volumeusage = 0; /* Some sort of metric for recent volume usage */
     char line[500];
     char idname[20]; 
     int nargs;
     int linenumber = 0;
+    char **argp;
 
     LogMsg(29, VolDebugLevel, stdout, "Entering Pass %c", type);
     MaxStride = 0;
     rewind(volumelist);
     while (fgets(line, sizeof(line), volumelist) != NULL) {
 	linenumber++;
-	if (line[0] == type) {
-	    register char **argp = args;
-	    GetArgs(line,argp,&nargs);
-	    name = &argp[0][1];
-	    if (type == 'P')
-		GetPartitionInfo(argp, nargs);
-	    else {
-		while (++argp,--nargs) {
-		    switch (**argp) {
-			case 'I':
-			    sscanf(&(*argp)[1], "%lx", &volume);
-			    break;
-			case 'H':
-			    sscanf(&(*argp)[1], "%x", &server);
-			    break;
-			case 'P':
-			    partition = &(*argp)[1];
-			    break;
-			case 'm':
-			    sscanf(&(*argp)[1], "%x", &minquota);
-			    break;
-			case 'M':
-			    sscanf(&(*argp)[1], "%x", &maxquota);
-			    break;
-			case 'U':
-			    sscanf(&(*argp)[1], "%x", &diskusage);
-			    break;
-			case 'W':
-			    sscanf(&(*argp)[1], "%lx", &readwrite);
-			    break;
-			case 'D':
-			    sscanf(&(*argp)[1], "%x", &copydate);
-			    break;
-			case 'B':
-			    sscanf(&(*argp)[1], "%x", &backupdate);
-			    break;
-			case 'C':
-			    sscanf(&(*argp)[1], "%x", &creationdate);
-			    break;
-			case 'A':
-			    sscanf(&(*argp)[1], "%x", &volumeusage);
-			    break;
-			default:
-			    LogMsg(0, VolDebugLevel, stdout, "Bad input field, line %d, field \"%s\": VolMakeVLDB aborted",
-				linenumber, *argp);
-			    return(VFAIL);
-		    }
-		}
-		sprintf(idname, "%lu", volume);
-		/* debugging hack: */
-		sprintf(rootname, "%lu", volume);
-		VolumeEntry(type, 0, idname, volume, server, readwrite, creationdate, copydate, backupdate);
-		VolumeEntry(type, 1, name, volume, server, readwrite, creationdate, copydate, backupdate);
-		fprintf(AllList, "%s %lx %s %s %u %u %u %c %u %u %u\n", name, volume, ServerName[server],
-			    partition, diskusage, minquota, maxquota, type, creationdate, copydate, volumeusage);
-		if (type == 'B') {
-		    register char *dot;
-		    if ( (dot = (char *) rindex(name, '.')) )
-			*dot = 0;
-		    fprintf(BackupList, "%s %lx %s %s\n", name, readwrite, ServerName[server], partition);
-		}
-		else if (type == 'W') {
-		    fprintf(RWList, "%s %lx %s %s %u %u %u\n", name, volume, ServerName[server],
-			    partition, diskusage, minquota, maxquota);
-		}
+	/* during the `p'artition scan we only report the number of lines in
+	 * the VolumeList */
+	if (type == 'P' || line[0] != type ) continue;
+
+	argp = args;
+	GetArgs(line,argp,&nargs);
+	name = &argp[0][1];
+
+	while (++argp,--nargs) {
+	    switch (**argp) {
+	    case 'I':
+		sscanf(&(*argp)[1], "%lx", &volume);
+		break;
+	    case 'H':
+		sscanf(&(*argp)[1], "%x", &server);
+		break;
+	    case 'W':
+		sscanf(&(*argp)[1], "%lx", &readwrite);
+		break;
+	    case 'D':
+		sscanf(&(*argp)[1], "%x", &copydate);
+		break;
+	    case 'B':
+		sscanf(&(*argp)[1], "%x", &backupdate);
+		break;
+	    case 'C':
+		sscanf(&(*argp)[1], "%x", &creationdate);
+		break;
+	    case 'P': /* partition = &(*argp)[1]; */
+	    case 'm': /* sscanf(&(*argp)[1], "%x", &minquota); */
+	    case 'M': /* sscanf(&(*argp)[1], "%x", &maxquota); */
+	    case 'U': /* sscanf(&(*argp)[1], "%x", &diskusage); */
+	    case 'A': /* sscanf(&(*argp)[1], "%x", &volumeusage); */
+		break;
+	    default:
+		LogMsg(0, VolDebugLevel, stdout, "Bad input field, line %d, field \"%s\": VolMakeVLDB aborted",
+		       linenumber, *argp);
+		return(VFAIL);
 	    }
 	}
+	sprintf(idname, "%lu", volume);
+	VolumeEntry(type, 0, idname, volume, server, readwrite, creationdate, copydate, backupdate);
+	VolumeEntry(type, 1, name, volume, server, readwrite, creationdate, copydate, backupdate);
     }
     return linenumber;
 }
@@ -394,12 +298,12 @@ static void VolumeEntry(char type, int byname, char *name, unsigned long volume,
 
 
 
-static void AddReadWriteEntry(register struct vldb *vnew, int byname,
+static void AddReadWriteEntry(struct vldb *vnew, int byname,
 			char *name, unsigned long volume, int server,
 			unsigned long readwrite, int creationdate,
 			int copydate, int backupdate)
 {
-    register struct vldb *old;
+    struct vldb *old;
     long olddate;
     old = Lookup(name, &olddate);
     if (old) {
@@ -413,13 +317,13 @@ static void AddReadWriteEntry(register struct vldb *vnew, int byname,
 	CheckRWindex(volume, name);
 }
 
-static void AddReadOnlyEntry(register struct vldb *vnew, int byname,
+static void AddReadOnlyEntry(struct vldb *vnew, int byname,
 			char *name, unsigned long volume, int server,
 			unsigned long readwrite, int creationdate,
 			int copydate, int backupdate)
 {
     long olddate;
-    register struct vldb *old;
+    struct vldb *old;
     int added = 0;
     char rwname[100];
     old = Lookup(name, &olddate);
@@ -440,7 +344,7 @@ static void AddReadOnlyEntry(register struct vldb *vnew, int byname,
     }
     sprintf(rwname, "%lu", readwrite);
     if (byname && added) {
-        register struct vldb *rwp;
+        struct vldb *rwp;
 	rwp = Lookup(rwname, &olddate);
         if (rwp) {
 	    AddAssociate(rwp, vnew);
@@ -451,13 +355,13 @@ static void AddReadOnlyEntry(register struct vldb *vnew, int byname,
     }
 }
 
-static void AddBackupEntry(register struct vldb *vnew, int byname,
+static void AddBackupEntry(struct vldb *vnew, int byname,
 		char *name, unsigned long volume, int server,
 		unsigned long readwrite, int creationdate,
 		int copydate, int backupdate)
 {
     long olddate;
-    register struct vldb *old;
+    struct vldb *old;
     char rwname[100];
     int added = 0;
     old = Lookup(name, &olddate);
@@ -475,7 +379,7 @@ static void AddBackupEntry(register struct vldb *vnew, int byname,
     }
     sprintf(rwname, "%lu", readwrite);
     if (byname && added) {
-        register struct vldb *rwp;
+        struct vldb *rwp;
 	rwp = Lookup(rwname, &olddate);
         if (rwp) {
 	    AddAssociate(rwp, vnew);
@@ -488,7 +392,7 @@ static void AddBackupEntry(register struct vldb *vnew, int byname,
 
 static struct vldb *Lookup(char *key, long *date)
 {
-    register struct vldb *p;
+    struct vldb *p;
     int index = HashString(key, vldbHashSize);
     for(p = &vldb_array[index]; p->key[0]; p += p->hashNext) {
 	if (strcmp(p->key, key) == 0) {
@@ -501,9 +405,9 @@ static struct vldb *Lookup(char *key, long *date)
     return 0;
 }
 
-static void Add(register struct vldb *vnew, long date)
+static void Add(struct vldb *vnew, long date)
 {
-    register struct vldb *p, *prev, *first;
+    struct vldb *p, *prev, *first;
     int index = HashString(vnew->key, vldbHashSize);
     LogMsg(19, VolDebugLevel, stdout, "Adding VLDB Entry for vol with key = %s", vnew->key);
     LogMsg(19, VolDebugLevel, stdout, "Add: index = %d, hashsize = %d", index, vldbHashSize);
@@ -520,7 +424,7 @@ static void Add(register struct vldb *vnew, long date)
     return;
 }
 
-static void Replace(register struct vldb *old, register struct vldb *vnew,
+static void Replace(struct vldb *old, struct vldb *vnew,
 							long date)
 {
     vnew->hashNext = old->hashNext;
@@ -528,9 +432,9 @@ static void Replace(register struct vldb *old, register struct vldb *vnew,
     Dates[vldbindex(old)] = date;
 }
 
-static void AddServer(register struct vldb *old, register struct vldb *vnew)
+static void AddServer(struct vldb *old, struct vldb *vnew)
 {
-    register int i;
+    int i;
     for (i = 0; i<old->nServers; i++) {
 	if (vnew->serverNumber[0] == old->serverNumber[i])
 	    return;
@@ -538,7 +442,7 @@ static void AddServer(register struct vldb *old, register struct vldb *vnew)
     old->serverNumber[old->nServers++] = vnew->serverNumber[0];
 }
 
-static void AddAssociate(register struct vldb *old, register struct vldb *vnew)
+static void AddAssociate(struct vldb *old, struct vldb *vnew)
 {
     old->volumeId[vnew->volumeType] = vnew->volumeId[vnew->volumeType];
 }
@@ -546,7 +450,7 @@ static void AddAssociate(register struct vldb *old, register struct vldb *vnew)
 static void CheckRWindex(unsigned long volume, char *name)
 {
     long date;
-    register struct vldb *idp, *namep;
+    struct vldb *idp, *namep;
     char idname[100];
     sprintf(idname, "%lu", volume);
     idp = Lookup(idname, &date);
@@ -558,34 +462,11 @@ static void CheckRWindex(unsigned long volume, char *name)
     RWindex[vldbindex(idp)] = namep;
 }
 
-static void GetPartitionInfo(char **argp, int nargs)
-{
-    char *partition;
-    char *server = 0;
-    unsigned int total, free;
-    partition = &argp[0][1];
-    while (++argp, --nargs) {
-	switch (**argp) {
-	    case 'H':
-		server = &argp[0][1];
-		break;
-	    case 'T':
-	        sscanf(&(*argp)[1], "%x", &total);
-		break;
-	    case 'F':
-	        sscanf(&(*argp)[1], "%x", &free);
-		break;
-	}
-    }
-    fprintf(PartList, "%s %s %u %u\n", server, partition, total, free);
-}
-
-static void GetArgs(register char *line,register char **args,
-						register int *nargs)
+static void GetArgs(char *line, char **args, int *nargs)
 {
     *nargs = 0;
     while (*line) {
-	register char *last = line;
+	char *last = line;
 	while (*line == ' ')
 	    line++;
 	if (*last == ' ')
