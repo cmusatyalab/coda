f07ea60c505890fda323c9d65a0798002795ac80
Jan Harkes <jaharkes@cs.cmu.edu>
2001-01-12 15:26:14 +0000

    Added debian packaging files.

---
+++ debian/coda-update.init.d
@@ -0,0 +1,64 @@
+#!/bin/sh
+#
+# Written by Miquel van Smoorenburg <miquels@cistron.nl>.
+# Modified for Debian GNU/Linux by Ian Murdock <imurdock@gnu.org>.
+# Modified for Debian by Christoph Lameter <clameter@debian.org>
+
+PATH=/bin:/usr/bin:/sbin:/usr/sbin
+RPC2PORTMAP=/usr/sbin/rpc2portmap
+UPDATECLNT=/usr/sbin/updateclnt
+UPDATESRV=/usr/sbin/updatesrv
+
+FLAGS="defaults 40"
+
+test -f $RPC2PORTMAP || exit 0
+test -f $UPDATECLNT || exit 0
+test -f $UPDATESRV || exit 0
+test -d $vicedir/db || exit 0
+test -f $vicedir/db/scm || exit 0
+
+SCM=`cat /vice/db/scm`
+HOST=`hostname`
+
+case "$1" in
+  start)
+    start-stop-daemon --start --verbose --exec $RPC2PORTMAP
+    [ $SCM != $HOST ] || start-stop-daemon --start --verbose --exec $UPDATESRV
+    start-stop-daemon --start --verbose --exec $UPDATECLNT
+    ;;
+  stop)
+    start-stop-daemon --stop --verbose --exec $UPDATECLNT
+    [ $SCM != $HOST ] || start-stop-daemon --stop --verbose --exec $UPDATESRV
+    start-stop-daemon --stop --verbose --exec $RPC2PORTMAP
+    ;;
+  #reload)
+    #
+    # If the daemon can reload its config files on the fly
+    # for example by sending it SIGHUP, do it here.
+    #
+    # If the daemon responds to changes in its config file
+    # directly anyway, make this a do-nothing entry.
+    #
+    # start-stop-daemon --stop --signal 1 --verbose --exec $RPC2PORTMAP
+    # [ $SCM != $HOST ] || start-stop-daemon --stop --signal 1 --verbose --exec $UPDATESRV
+    # start-stop-daemon --stop --signal 1 --verbose --exec $UPDATECLNT
+    # ;;
+  restart|force-reload)
+  #
+  # If the "reload" option is implemented, move the "force-reload"
+  # option to the "reload" entry above. If not, "force-reload" is
+  # just the same as "restart".
+  #
+    start-stop-daemon --stop --verbose --exec $UPDATECLNT
+    [ $SCM != $HOST ] || start-stop-daemon --stop --verbose --exec $UPDATESRV
+    sleep 1
+    [ $SCM != $HOST ] || start-stop-daemon --start --verbose --exec $UPDATESRV
+    start-stop-daemon --start --verbose --exec $UPDATECLNT
+    ;;
+  *)
+    echo "Usage: /etc/init.d/coda-update {start|stop|restart|force-reload}"
+    exit 1
+    ;;
+esac
+
+exit 0
---
+++ debian/init.d
@@ -0,0 +1,47 @@
+#!/bin/sh
+#
+# Written by Miquel van Smoorenburg <miquels@cistron.nl>.
+# Modified for Debian GNU/Linux by Ian Murdock <imurdock@gnu.org>.
+# Modified for Debian by Christoph Lameter <clameter@debian.org>
+
+PATH=/bin:/usr/bin:/sbin:/usr/sbin
+DAEMON=/usr/sbin/venus
+
+FLAGS="defaults 50"
+
+test -f $DAEMON || exit 0
+
+case "$1" in
+  start)
+    start-stop-daemon --start --verbose --background --exec $DAEMON
+    ;;
+  stop)
+    start-stop-daemon --stop --verbose --exec $DAEMON
+    ;;
+  #reload)
+    #
+    # If the daemon can reload its config files on the fly
+    # for example by sending it SIGHUP, do it here.
+    #
+    # If the daemon responds to changes in its config file
+    # directly anyway, make this a do-nothing entry.
+    #
+    # start-stop-daemon --stop --signal 1 --verbose --exec $DAEMON
+    # ;;
+  restart|force-reload)
+  #
+  # If the "reload" option is implemented, move the "force-reload"
+  # option to the "reload" entry above. If not, "force-reload" is
+  # just the same as "restart".
+  #
+    start-stop-daemon --stop --verbose --exec $DAEMON
+    sleep 1
+    start-stop-daemon --start --verbose --background --exec $DAEMON
+    ;;
+  *)
+    echo "Usage: /etc/init.d/coda-client {start|stop|restart|force-reload}"
+    exit 1
+    ;;
+esac
+
+exit 0
