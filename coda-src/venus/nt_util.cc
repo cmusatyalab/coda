a934418ca287729e75f47b4b92ef3f981246ede2
Phil Nelson <phil@cs.wwu.edu>
2001-07-21 03:16:01 +0000

    Code to integrate mount and relayNT into venus.  Both work.

---
+++ coda-src/venus/nt_util.cc
@@ -0,0 +1,246 @@
+/* BLURB gpl
+
+                           Coda File System
+                              Release 5
+
+          Copyright (c) 1987-1999 Carnegie Mellon University
+                  Additional copyrights listed below
+
+This  code  is  distributed "AS IS" without warranty of any kind under
+the terms of the GNU General Public Licence Version 2, as shown in the
+file  LICENSE.  The  technical and financial  contributors to Coda are
+listed in the file CREDITS.
+
+                        Additional copyrights
+                           none currently
+
+#*/
+
+#ifdef __CYGWIN32__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <winbase.h>
+
+#include "coda_string.h"
+#include "coda.h"
+#include "util.h"
+
+#ifdef __cplusplus
+}
+#endif
+
+#include "nt_util.h"
+
+/*
+ * NT specific routines 
+ *
+ */
+
+// Mounts ...
+
+// Parameters for nd_do_mounts ... both for direct call
+// and via CreateThread.
+static char drive;
+static int  mount;
+
+int
+wcslen (PWCHAR wstr)
+{
+    int len = 0;
+    while (*wstr++) len++;
+    return len;
+}
+
+
+static DWORD
+nt_do_mounts (void *junk)
+{
+    HANDLE h;
+    OW_PSEUDO_MOUNT_INFO info;
+    DWORD nBytesReturned;
+    CHAR outBuf[4096];
+    DWORD d;
+    int n;
+    int ctlcode = OW_FSCTL_DISMOUNT_PSEUDO;
+    
+    WCHAR link[20] = L"\\??\\X:";  
+    
+    // Parameters ...
+    link[4] = (short)drive;
+    if (mount)
+	ctlcode = OW_FSCTL_MOUNT_PSEUDO;
+    
+    d = DefineDosDevice(DDD_RAW_TARGET_PATH, "codadev", "\\Device\\codadev");
+    if ( d == 0 ) {
+	eprint ("DDD failed, mount failed.\n");
+	return 1; 
+    }
+
+    h = CreateFile ("\\\\.\\codadev", GENERIC_READ | GENERIC_WRITE,
+		    FILE_SHARE_READ | FILE_SHARE_WRITE, NULL,
+		    OPEN_EXISTING, 0, NULL);
+
+    if (h == INVALID_HANDLE_VALUE) {
+	eprint ("CreateFile failed, mount failed.\n");
+	return 1; 
+    } 
+    
+    // Set up the info for the DeviceIoControl.
+    info.PseudoVolumeHandle = (HANDLE*)1;
+    info.PseudoDeviceName = L"\\Device\\coda";
+    info.PseudoDeviceNameLength =
+	wcslen(info.PseudoDeviceName) * sizeof(WCHAR);
+    info.PseudoLinkName = link;
+    info.PseudoLinkNameLength = wcslen(info.PseudoLinkName) * sizeof(WCHAR);
+    
+    d = DeviceIoControl(h, ctlcode, &info, sizeof(info), NULL, 0,
+			&nBytesReturned, NULL);
+    if (d)
+        return 1;
+    return 0;
+}
+
+void nt_umount (char *drivename)
+{
+    drive = drivename[0];
+    mount = 0;
+    (void) nt_do_mounts (NULL);
+}
+
+void
+nt_mount (char *drivename)
+{
+    HANDLE h;
+
+    nt_umount (drivename);
+    mount = 1;
+    
+    h = CreateThread(0, 0, nt_do_mounts, NULL, 0, NULL);
+
+    if (!h)
+	eprint ("CreateThread failed.  Mount unsuccessful.\n");
+
+    CloseHandle (h);
+}
+
+//
+// kernel -> venus ... using a socket pair.
+//
+
+static int sockfd;
+static volatile int doexit;
+
+static DWORD
+listen_kernel (void *junk)
+{
+    HANDLE h;
+    int rc;
+    DWORD bytesret;
+    char outbuf[VC_MAXMSGSIZE];
+
+    // Get the device ready;
+    rc = DefineDosDevice(DDD_RAW_TARGET_PATH, "codadev", "\\Device\\codadev");
+    if ( rc == 0 ) {
+	eprint ("DDD failed, listen_kernel failed.\n");
+	return 1; 
+    }
+
+    h = CreateFile ("\\\\.\\codadev", GENERIC_READ | GENERIC_WRITE,
+		    FILE_SHARE_READ | FILE_SHARE_WRITE, NULL,
+		    OPEN_EXISTING, 0, NULL);
+
+    if (h == INVALID_HANDLE_VALUE) {
+	eprint ("CreateFile failed, listen_kernel failed.");
+	return 1; 
+    } 
+
+    while (1) {
+	// Do a device ioctl.
+	bytesret = 0;
+	rc = DeviceIoControl (h, CODA_FSCTL_FETCH, NULL, 0, outbuf,
+			      VC_MAXMSGSIZE, &bytesret, NULL);
+	if (rc) {
+	    if (bytesret > 0) {
+		write (sockfd, outbuf, bytesret);
+	    }
+	} else {
+	    eprint ("listen_kernel: fetch failed");
+	}
+	if (doexit)
+	  ExitThread(0);
+    }
+}
+
+// "public" interface for ipc
+
+static HANDLE kerndev;
+static HANDLE kernelmon;
+
+int nt_initialize_ipc (int sock)
+{
+    int rc;
+
+    // Get the device ready for writing to kernel
+    rc = DefineDosDevice(DDD_RAW_TARGET_PATH, "codadev", "\\Device\\codadev");
+    if ( rc == 0 ) {
+	eprint ("nt_initialize_ipc: DDD failed.");
+	return 0; 
+    }
+
+    kerndev = CreateFile ("\\\\.\\codadev", GENERIC_READ | GENERIC_WRITE,
+			  FILE_SHARE_READ | FILE_SHARE_WRITE, NULL,
+			  OPEN_EXISTING, 0, NULL);
+
+    if (kerndev == INVALID_HANDLE_VALUE) {
+	eprint ("nt_initialize_ipc: CreateFile failed.");
+	return 0; 
+    } 
+
+    // Other initialization
+    sockfd = sock;
+    doexit = 0;
+
+    // Start the kernel monitor
+    kernelmon = CreateThread (0, 0, listen_kernel, NULL, 0, NULL);
+    if (kernelmon == NULL) {
+	return 0;
+    }
+
+    // All was successful 
+    return 1;
+}
+
+int nt_msg_write (char *buf, int size)
+{
+    int rc;
+    DWORD bytesret;
+
+    //    eprint ("nt_msg_write: Start\n");
+    rc = DeviceIoControl (kerndev, CODA_FSCTL_ANSWER, buf, size, NULL, 0,
+			  &bytesret, NULL);
+    //    eprint ("nt_msg_write: End\n");
+    if (!rc)
+	return 0;
+    
+    return size;
+}
+
+void nt_stop_ipc (void)
+{
+    (void) TerminateThread (kernelmon,  0);
+    CloseHandle(kernelmon);
+    doexit = 1;
+}
+
+
+#endif
---
+++ coda-src/venus/nt_util.h
@@ -0,0 +1,60 @@
+/* BLURB gpl
+
+                           Coda File System
+                              Release 5
+
+          Copyright (c) 1987-1999 Carnegie Mellon University
+                  Additional copyrights listed below
+
+This  code  is  distributed "AS IS" without warranty of any kind under
+the terms of the GNU General Public Licence Version 2, as shown in the
+file  LICENSE.  The  technical and financial  contributors to Coda are
+listed in the file CREDITS.
+
+                        Additional copyrights
+                           none currently
+
+#*/
+
+#ifdef __CYGWIN32__
+#include <windows.h>
+#include <winbase.h>
+#include <winioctl.h>
+
+// Defines needed for DeviceIoControls from OSR headers
+
+#define OW_FSCTL_MOUNT_PSEUDO           CTL_CODE(FILE_DEVICE_FILE_SYSTEM, 3031, METHOD_BUFFERED, FILE_WRITE_ACCESS)
+#define OW_FSCTL_DISMOUNT_PSEUDO        CTL_CODE(FILE_DEVICE_FILE_SYSTEM, 3033, METHOD_NEITHER, FILE_WRITE_ACCESS)
+
+//
+// Pseudo mount/dismount data structures
+//
+
+typedef struct {
+    HANDLE PseudoVolumeHandle;
+    USHORT PseudoDeviceNameLength;
+    PWCHAR PseudoDeviceName;
+    USHORT PseudoLinkNameLength;
+    PWCHAR PseudoLinkName;
+} OW_PSEUDO_MOUNT_INFO, *POW_PSEUDO_MOUNT_INFO;
+
+// XXX broken ... should use CTL_CODE like above.. should match
+// ntfsd/coda/coda_nt.h
+//
+#define CODA_FSCTL_ANSWER 0x1002
+#define CODA_FSCTL_FETCH  0x1003
+
+// prototypes
+
+void nt_mount (char *drivename);
+void nt_umount (char *drivename);
+
+int nt_initialize_ipc (int sock);
+int nt_msg_write (char *buf, int size);
+void nt_stop_ipc (void);
+
+// NTipc switch!
+
+#define NEW_NT_IPC
+
+#endif
