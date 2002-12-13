5a47a82569097f9026b4c81bda64ced20da93a19
Jan Harkes <jaharkes@cs.cmu.edu>
2002-12-13 16:18:20 +0000

    updated changelog. bumped venus version.

--- coda-src/venus/venus.version.h
+++ coda-src/venus/venus.version.h
@@ -23,6 +23,6 @@ listed in the file CREDITS.
 /*  *****  Venus Version Numbers *****  */
 const int VenusMajorVersion = 5;
 const int VenusMinorVersion = 3;
-const int VenusReleaseVersion = 19;
+const int VenusReleaseVersion = 20;
 
 #endif
