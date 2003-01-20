5ec6a58beb71c4923f5227bc6cdd5d90246f8a32
Jan Harkes <jaharkes@cs.cmu.edu>
2003-01-20 16:44:56 +0000

    Some cleanups for the spec file from Rafael Jesus Alcantara Perez

--- tools/build-coda-rpm.pl
+++ tools/build-coda-rpm.pl
@@ -2,34 +2,17 @@
 
 use Getopt::Long;
 
-GetOptions("debug!" => \$debug, "kerberos!" => \$kerberos, "snapshot!" => \$snapshot, "rel=s" => \$rel, "version=s" => \$version);
+GetOptions("debug!" => \$debug, "rel=s" => \$rel, "version=s" => \$version);
 
 if ( ! $rel || ! $version ) {
-    print "Usage $0 --rel=release --version=cvs-version  {--debug} {--kerberos} {--snapshot}\n";
+    print "Usage $0 --rel=release --version=cvs-version  {--debug}\n";
     exit 1;
-}
-
-if ( $snapshot == 1 ) {
-    $tversion = $version . "-" . $rel
-} else {
-    $tversion = $version
 }
 
 if ( $debug == 1 ) {
     $codaname = "coda-debug" ;
 } else {
     $codaname = "coda";
-}
-
-if ( $kerberos == 1 ) {
-  $codaname = $codaname . "-kerberos";
-  $auth2name="kauth2";
-  $clogname="kclog";
-  $auname="kau";
-} else {
-  $auth2name="auth2";
-  $clogname="clog";
-  $auname="au";
 }
 
 print "DEBUG is $debug, name is $codaname\n";
@@ -42,16 +25,9 @@ print "Will create specfile $specfile\n";
 open(SPEC, ">$specfile");
 while ( <> ) {
     ~ s/\@REL\@/$rel/g;
-    ~ s/\@CVER\@/$version/g;
-    ~ s/\@VERSION\@/$tversion/g;
-    ~ s/\@KVER\@/$kernel/g;
-    ~ s/\@KV\@/$kv/g;
-    ~ s/\@LIBC\@/$libc/g;
+    ~ s/\@VERSION\@/$version/g;
     ~ s/\@DEBUG\@/$debug/g;
     ~ s/\@CODA\@/$codaname/g;
-    ~ s/\@AUTH2\@/$auth2name/g;
-    ~ s/\@CLOG\@/$clogname/g;
-    ~ s/\@AU\@/$auname/g;
     print SPEC $_;
     print $_
 }
