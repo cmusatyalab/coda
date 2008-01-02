8fee6adba0302aeca5308587c6dfd57bfe7ec459
Phil Nelson <phil@cs.wwu.edu>
2008-01-02 16:05:53 -0500

    Add the post install script for the cygwin package.

---
+++ tools/cyg-postinst.sh
@@ -0,0 +1,37 @@
+# Final installation script ... build /usr/coda with proper permissions.
+# Parameters:  None:
+
+PATH=/bin:/usr/bin:/usr/sbin
+CODACONFPATH=/etc/coda
+export CODACONFPATH
+
+# default set up ...
+venus-setup coda.cs.cmu.edu 250000 n
+
+if ! test -d /usr/coda ; then
+  mkdir /usr/coda
+fi
+if ! test -d /usr/coda/venus.cache ; then
+  mkdir /usr/coda/venus.cache
+fi
+if ! test -d /usr/coda/spool ; then
+  mkdir /usr/coda/spool
+fi
+if ! test -d /usr/coda/etc ; then
+  mkdir /usr/coda/etc
+fi
+
+touch /usr/coda/{DATA,LOG}
+touch /usr/coda/venus.cache/INIT
+
+chmod 755 /usr/coda
+chmod 755 /usr/coda/etc
+chmod 770 /usr/coda/{spool,venus.cache}
+chmod 600 /usr/coda/{DATA,LOG}
+
+chown SYSTEM:Administrators /usr/coda/{DATA,LOG,etc,spool,venus.cache}
+chown SYSTEM:Administrators /usr/coda
+
+# install venus as a service
+cygrunsrv -R venus
+cygrunsrv -I venus -p /usr/sbin/venus -o -t manual
