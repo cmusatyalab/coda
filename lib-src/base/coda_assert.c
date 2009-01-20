5c803b6f8789357e2f898737a2f8ad49bb27d793
Jan Harkes <jaharkes@cs.cmu.edu>
2009-01-20 17:32:24 -0500

    Add support for dumping the stack trace when asserting.
    Relies on glibc specific functions so only useful on Linux.
    Currently unconditionally links with -rdynamic which increases the size
    of executables by including all non-static symbols.

--- lib-src/base/coda_assert.c
+++ lib-src/base/coda_assert.c
@@ -1,21 +1,28 @@
 /* BLURB lgpl
 
-                           Coda File System
-                              Release 6
+			Coda File System
+			    Release 6
 
-          Copyright (c) 1987-2003 Carnegie Mellon University
-                  Additional copyrights listed below
+	Copyright (c) 1987-2009 Carnegie Mellon University
+		Additional copyrights listed below
 
 This  code  is  distributed "AS IS" without warranty of any kind under
 the  terms of the  GNU  Library General Public Licence  Version 2,  as
 shown in the file LICENSE. The technical and financial contributors to
 Coda are listed in the file CREDITS.
 
-                        Additional copyrights
-                           none currently
+		    Additional copyrights
+			none currently
 
 #*/
 
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#ifdef HAVE_EXECINFO_H
+#include <execinfo.h>
+#endif
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -28,8 +35,22 @@ int   coda_assert_action = CODA_ASSERT_SLEEP;
 void
 coda_assert(const char *pred, const char *file, int line)
 {
-    fprintf(stderr,"Assertion failed: %s, file \"%s\", line %d\n", pred, file, line);
+#ifdef HAVE_BACKTRACE
+#define BT_BUF_SIZE 64
+    void *bt_buf[BT_BUF_SIZE];
+    int nptrs;
+
+    fprintf(stderr,"Assertion failed: %s\n***BackTrace***\n", pred);
     fflush(stderr);
+
+    nptrs = backtrace(bt_buf, BT_BUF_SIZE);
+    backtrace_symbols_fd(bt_buf, nptrs, STDERR_FILENO);
+
+#else
+    fprintf(stderr,"Assertion failed: %s, file \"%s\", line %d\n",
+	    pred, file, line);
+    fflush(stderr);
+#endif
 
     if (coda_assert_cleanup) (coda_assert_cleanup)();
 
@@ -42,14 +63,14 @@ coda_assert(const char *pred, const char *file, int line)
 	fprintf(stderr, "Sleeping forever.  You may use gdb to attach to process %d.",
 		(int)getpid());
 	fflush(stderr);
-        for (;;)
+	for (;;)
 	     sleep(1);
 	break;
 
     case CODA_ASSERT_EXIT:
 	fprintf(stderr, "EXITING! Bye!\n");
 	fflush(stderr);
-    	exit(77);
+	exit(77);
 	break;
 
     case CODA_ASSERT_ABORT:
