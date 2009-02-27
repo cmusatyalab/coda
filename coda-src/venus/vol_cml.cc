f13cf0361d97c1c59c6c114925b5cad7899da75a
Jan Harkes <jaharkes@cs.cmu.edu>
2009-02-27 11:48:37 -0500

    Interesting strrchr related build failure.
    strrchr is defined as "char *strrchr(const char *s, int c)", however
    it returns a pointer into the string 's' that was passed as the first
    argument so it really should have been defined as returning const char*.
    
    Looks like recent fedora systems are getting this right and as such fail
    during the build, we dropped the const when assigning the result.

--- coda-src/venus/vol_cml.cc
+++ coda-src/venus/vol_cml.cc
@@ -3606,7 +3606,8 @@ static int WriteLinks(struct DirEntry *de, void *arg)
 {
     VnodeId vnode;
     Unique_t vunique;
-    char *name = de->name, *comp;
+    char *name = de->name;
+    const char *comp;
     size_t prefixlen;
     char namebuf[CODA_MAXPATHLEN];
     struct WriteLinksHook *hook = (struct WriteLinksHook *)arg;
