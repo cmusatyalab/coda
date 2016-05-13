958276e1be221bfd62318454bbadda0d78442025
Jan Harkes <jaharkes@cs.cmu.edu>
2016-05-13 14:23:41 -0400

    We cannot traverse the same va_list twice.
    On a recent Debian system the client would reliably segfault when
    binding to a new realm. Resetting the va_list between the stdout
    and stderr vfprintfs avoids the segfault.

--- coda-src/util/util.c
+++ coda-src/util/util.c
@@ -3,7 +3,7 @@
                            Coda File System
                               Release 6
 
-          Copyright (c) 1987-2003 Carnegie Mellon University
+          Copyright (c) 1987-2016 Carnegie Mellon University
                   Additional copyrights listed below
 
 This  code  is  distributed "AS IS" without warranty of any kind under
@@ -195,6 +195,9 @@ void eprint(const char *fmt, ...)
         vfprintf(stdout, fmt, ap);
         printf(stdout, '\n');
 	fflush(stdout);
+        va_end(ap);
+
+	va_start(ap, fmt);
 	PrintTimeStamp(stderr);
         vfprintf(stderr, fmt, ap);
         printf(stderr, '\n');
