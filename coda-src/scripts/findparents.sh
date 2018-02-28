151a693c54060b679946b92e651e404e382318c6
Jan Harkes <jaharkes@cs.cmu.edu>
2018-02-28 16:17:19 -0500

    Used shellcheck to clean up the scripts
    Hopefully didn't break too much.

--- coda-src/scripts/findparents.sh
+++ coda-src/scripts/findparents.sh
@@ -1,14 +1,13 @@
 #! /bin/sh
 
 
-grep  backup VolumeList | awk '{ print $8 }'  | sed 's/^W//' > /tmp/parents
+grep backup VolumeList | awk '{ print $8 }'  | sed 's/^W//' > /tmp/parents
 
 echo "No parents for:"
-for i in `cat /tmp/parents` ; do
+for i in $(cat /tmp/parents) ; do
 #    echo Processing $i
-    grep -q I$i /vice/vol/VolumeList
-    if [ x$? != x0 ]; then 
-	grep W$i /vice/vol/VolumeList | awk '{ print $2 " " $1}' | sed 's/^I//' | sed 's/ B/ /'
+    grep -q "I$i" /vice/vol/VolumeList
+    if [ $? != 0 ]; then 
+	grep "W$i" /vice/vol/VolumeList | awk '{ print $2 " " $1}' | sed 's/^I//' | sed 's/ B/ /'
     fi
-
 done
