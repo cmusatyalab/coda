324a0ab43b29aa337dcdf5a0ebe43d5c9d528da0
Pedro Cuadra <pcuadrac@cs.cmu.edu>
2018-10-31 12:18:41 -0400

    Doxyfy copyfile.h

--- lib-src/base/copyfile.h
+++ lib-src/base/copyfile.h
@@ -36,9 +36,36 @@ Coda are listed in the file CREDITS.
 extern "C" {
 #endif
 
-/* these functions are used to copy files, they return -1 on errors */
+/**
+ * Copy a segment of a file to another file
+ *
+ * @param infd  file descriptor of the source file
+ * @param outfd file descriptor of the destination file
+ * @param pos   offset within the source file
+ * @param count amount of bytes to be copied
+ * 
+ * @return -1 on errors and 0 otherwise
+ */
 int copyfile_seg(int infd, int outfd, uint64_t pos, int64_t count);
+
+/**
+ * Copy a file to another file
+ *
+ * @param fromfd file descriptor of the source file
+ * @param tofd   file descriptor of the destination file
+ * 
+ * @return -1 on errors and 0 otherwise
+ */
 int copyfile(int fromfd, int tofd);
+
+/**
+ * Copy a file to another file
+ *
+ * @param fromname file path of the source file
+ * @param toname   file path of the destination file
+ * 
+ * @return -1 on errors and 0 otherwise
+ */
 int copyfile_byname(const char *fromname, const char *toname);
 
 #ifdef __cplusplus
