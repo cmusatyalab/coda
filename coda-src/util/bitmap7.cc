f5bf9ae924d04eba40f145685e1653b382174e5d
Pedro Cuadra <pcuadrac@cs.cmu.edu>
2019-01-11 16:59:18 -0500

    Fix formatting of bitmap7

--- coda-src/util/bitmap7.cc
+++ coda-src/util/bitmap7.cc
@@ -260,7 +260,8 @@ void bitmap7::CopyRange(int start, int len, bitmap7 &b)
 
     /* Copy all the bytes in between */
     memcpy(&b.map[start_byte], &map[start_byte], bulk_len);
-    if (recoverable) rvmlib_set_range(&b.map[start_byte], sizeof(char) * bulk_len);
+    if (recoverable)
+        rvmlib_set_range(&b.map[start_byte], sizeof(char) * bulk_len);
 
     /* Copy the values from before the first copied byte */
     for (int i = start; i & 0x7; i++) {
@@ -293,7 +294,8 @@ void bitmap7::SetRangeValue(int start, int len, int value)
 
     /* Copy all the bytes in between */
     memset(&map[start_byte], bulk_value, bulk_len);
-    if (recoverable) rvmlib_set_range(&map[start_byte], sizeof(char) * bulk_len);
+    if (recoverable)
+        rvmlib_set_range(&map[start_byte], sizeof(char) * bulk_len);
 
     /* Copy the values from before the first copied byte */
     for (int i = start; i & 0x7; i++) {
