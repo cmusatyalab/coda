5c72b254b58f934cf7d690801b2359d6ebc006a2
Pedro Cuadra <pcuadrac@cs.cmu.edu>
2019-04-23 16:47:23 -0400

    Improve RVM tests message printing

--- test-src/unit/include/testing/memory.h
+++ test-src/unit/include/testing/memory.h
@@ -31,6 +31,17 @@ extern "C" {
 
 #include "gtest/gtest.h"
 
+#define PRINT_FAILURE_MESSAGE                                               \
+    const ::testing::TestInfo *const test_info =                            \
+        ::testing::UnitTest::GetInstance()->current_test_info();            \
+    const ::testing::TestResult *res = test_info->result();                 \
+    for (int i = 0; i < res->total_part_count(); i++) {                     \
+        const testing::TestPartResult part_res = res->GetTestPartResult(i); \
+        std::cout << part_res.file_name() << ":" << part_res.line_number()  \
+                  << " Failure" << std::endl;                               \
+        std::cout << part_res.summary() << std::endl;                       \
+    }
+
 #define TEST_FOR_MEMORY_LEAKS_SETUP                                   \
     unsigned long base_definite, base_dubious, base_reachable,        \
         base_suppressed;                                              \
@@ -47,15 +58,18 @@ extern "C" {
     EXPECT_EQ(base_reachable, reachable) << "Memory Leak found";  \
     EXPECT_EQ(base_suppressed, suppressed) << "Memory Leak found";
 
-#define RVM_TEST_BODY(test_case_name, test)                          \
-    SCOPED_TRACE("Memory Leaks Found");                              \
+#define RVM_LAUNCH_IN_INSTANCE(function)                             \
     ASSERT_EXIT(({                                                   \
                     TEST_FOR_MEMORY_LEAKS_SETUP                      \
-                    test_case_name##_##test##_RvmTest();             \
+                    function();                                      \
                     VALGRIND_DO_LEAK_CHECK;                          \
                     VALGRIND_COUNT_LEAKS(leaked, dubious, reachable, \
                                          suppressed);                \
                     TEST_FOR_MEMORY_LEAKS_CHECK                      \
+                    ::testing::Test::TearDownTestCase();             \
+                    if (::testing::Test::HasFailure()) {             \
+                        PRINT_FAILURE_MESSAGE                        \
+                    }                                                \
                     exit(::testing::Test::HasFailure() ? 1 : 0);     \
                 }),                                                  \
                 ::testing::ExitedWithCode(0), ".*");
@@ -63,14 +77,20 @@ extern "C" {
 /* Google test wrapper to support memory leaks testing for forked
  * processes. Tests that require RVM should fork/clone since RVM
  * is NOT reentrant. */
-#define RVM_TEST(test_case_name, test)                                 \
-    static void test_case_name##_##test##_RvmTest();                   \
-    TEST(test_case_name, test) { RVM_TEST_BODY(test_case_name, test) } \
+#define RVM_TEST(test_case_name, test)                            \
+    static void test_case_name##_##test##_RvmTest();              \
+    TEST(test_case_name, test)                                    \
+    {                                                             \
+        RVM_LAUNCH_IN_INSTANCE(test_case_name##_##test##_RvmTest) \
+    }                                                             \
     static void test_case_name##_##test##_RvmTest()
 
-#define RVM_TEST_F(test_case_name, test)                                 \
-    static void test_case_name##_##test##_RvmTest();                     \
-    TEST_F(test_case_name, test) { RVM_TEST_BODY(test_case_name, test) } \
+#define RVM_TEST_F(test_case_name, test)                          \
+    static void test_case_name##_##test##_RvmTest();              \
+    TEST_F(test_case_name, test)                                  \
+    {                                                             \
+        RVM_LAUNCH_IN_INSTANCE(test_case_name##_##test##_RvmTest) \
+    }                                                             \
     static void test_case_name##_##test##_RvmTest()
 
 #endif /* _CODA_MEMORY_H_ */
