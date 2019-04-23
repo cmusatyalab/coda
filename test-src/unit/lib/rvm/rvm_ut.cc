3a74a60f4a5741bf18baebf4bd064326296ce5e3
Pedro Cuadra <pcuadrac@cs.cmu.edu>
2019-04-23 16:53:41 -0400

    Minor refactoring of RVM unittests

--- test-src/unit/lib/rvm/rvm_ut.cc
+++ test-src/unit/lib/rvm/rvm_ut.cc
@@ -23,7 +23,16 @@ namespace
 class RvmTest : public ::testing::Test {
 };
 
-using RvmDeathTest = RvmTest;
+class RvmDeathTest : public ::testing::Test {
+public:
+    char *tmp_data;
+    int tmp_data_size;
+    static char *data_dev;
+
+    void map_and_populate_region();
+};
+
+char *RvmDeathTest::data_dev = "rvm_test.data";
 
 TEST(RvmTest, alloc_options)
 {
@@ -42,23 +51,23 @@ TEST(RvmTest, terminate)
     ASSERT_EQ(rvm_terminate(), RVM_EINIT);
 }
 
-RVM_TEST(RvmDeathTest, init_default)
+RVM_TEST_F(RvmDeathTest, init_default)
 {
-    ASSERT_EQ(rvm_initialize(RVM_VERSION, NULL), RVM_SUCCESS);
-    ASSERT_EQ(rvm_terminate(), RVM_SUCCESS);
+    EXPECT_EQ(rvm_initialize(RVM_VERSION, NULL), RVM_SUCCESS);
+    EXPECT_EQ(rvm_terminate(), RVM_SUCCESS);
 }
 
-RVM_TEST(RvmDeathTest, init_minimal)
+RVM_TEST_F(RvmDeathTest, init_minimal)
 {
     const int TRUNCATE_VAL      = 30; /* truncate threshold */
     rvm_options_t *init_options = rvm_malloc_options();
-    ASSERT_TRUE(init_options);
+    EXPECT_TRUE(init_options);
 
     init_options->truncate = TRUNCATE_VAL;
     init_options->flags |= RVM_ALL_OPTIMIZATIONS;
     init_options->flags |= RVM_MAP_PRIVATE;
 
-    ASSERT_EQ(rvm_initialize(RVM_VERSION, init_options), RVM_SUCCESS);
+    EXPECT_EQ(rvm_initialize(RVM_VERSION, init_options), RVM_SUCCESS);
 
     rvm_free_options(init_options);
     ASSERT_EQ(rvm_terminate(), RVM_SUCCESS);
