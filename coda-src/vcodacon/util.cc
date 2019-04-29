907d051d6bd397480fcc5cf2df782600ca3daa41
Pedro Cuadra <pcuadrac@cs.cmu.edu>
2019-04-29 11:35:10 -0400

    Fix license BLURB dates

--- coda-src/vcodacon/util.cc
+++ coda-src/vcodacon/util.cc
@@ -1,9 +1,9 @@
 /* BLURB gpl
 
                             Coda File System
-                               Release 7
+                               Release 6
 
-           Copyright (c) 1987-2019 Carnegie Mellon University
+           Copyright (c) 1987-2003 Carnegie Mellon University
                    Additional copyrights listed below
 
  This  code  is  distributed "AS IS" without warranty of any kind under
