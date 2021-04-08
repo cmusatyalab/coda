decd944e7afce3cd6ef4ad6eb164d086c7db1af9
Jan Harkes <jaharkes@cs.cmu.edu>
2021-04-07 23:03:34 -0400

    Pull common code into a single cfs wrapper function

--- python/codafs/cfs.py
+++ python/codafs/cfs.py
@@ -24,30 +24,37 @@ class NotCodaFS(FileNotFoundError):
     """ Exception 'trying to do Coda operation on non-Coda file system' """
 
 
-_commands = {}
+# Cached path to the cfs executable
+_CFS_COMMAND = None
 
 
-# test if required binaries exist
-def _which(name, hint):
-    if name not in _commands:
-        path = find_executable(name)
-        if path is None:
-            raise FileNotFoundError("Cannot find {}, {}".format(name, hint))
-        _commands[name] = path
-    return _commands[name]
+def cfs(*args):
+    """ locate cfs command and run it with the given arguments """
+    global _CFS_COMMAND  # pylint: disable=global-statement
+    if _CFS_COMMAND is None:
+        _CFS_COMMAND = find_executable("cfs")
+        if _CFS_COMMAND is None:
+            raise FileNotFoundError(
+                "Cannot find cfs, check your Coda client installation"
+            )
+
+    # try to run cfs command
+    try:
+        result = subprocess.check_output(
+            (_CFS_COMMAND,) + args, stderr=subprocess.STDOUT
+        )
+    except subprocess.CalledProcessError as exc:
+        raise NotCodaFS("cfs {} failed".format(args[0])) from exc
+
+    return result.decode("ascii")
 
 
 def getfid(path):
     """ return Coda file identifier for specified path """
-    cfs = _which("cfs", "check your Coda client installation")
-    try:
-        result = subprocess.check_output([cfs, "getfid", str(path)])
-    except subprocess.CalledProcessError:
-        raise NotCodaFS("cfs getfid failed on {}".format(path))
+    result = cfs("getfid", str(path))
 
     match = re.match(
-        r"^FID = ([0-9a-fA-F]+)\.([0-9a-fA-F]+)\.([0-9a-fA-F]+)@(\S+)",
-        result.decode("ascii"),
+        r"^FID = ([0-9a-fA-F]+)\.([0-9a-fA-F]+)\.([0-9a-fA-F]+)@(\S+)", result
     )
     if match is None:
         raise NotCodaFS("cfs getfid unexpected output on {}".format(path))
@@ -56,15 +63,8 @@ def getfid(path):
 
 def listvol(path):
     """ return volume_id, name tuple for specified path """
-    cfs = _which("cfs", "check your Coda client installation")
-    try:
-        result = subprocess.check_output(
-            [cfs, "listvol", str(path)], stderr=subprocess.STDOUT
-        )
-    except subprocess.CalledProcessError:
-        raise NotCodaFS("cfs listvol failed on {}".format(path))
+    result = cfs("listvol", str(path))
 
-    result = result.decode("ascii")
     if result.endswith(": Permission denied\n"):
         raise PermissionError("cfs listvol permission denied on {}".format(path))
 
@@ -76,23 +76,16 @@ def listvol(path):
 
 def listacl(path):
     """ returns an ACL (list of user/group, rights tuples) for the specified path """
-    cfs = _which("cfs", "check your Coda client installation")
-    try:
-        result = subprocess.check_output([cfs, "listacl", str(path)])
-    except subprocess.CalledProcessError:
-        raise NotCodaFS("cfs listvol failed on {}".format(path))
+    result = cfs("listacl", str(path))
 
     return [
         AclEntry(*entry)
-        for entry in re.findall(
-            r"\s*(\S+)\s+(-?r?l?i?d?w?k?a?)", result.decode("ascii")
-        )
+        for entry in re.findall(r"\s*(\S+)\s+(-?r?l?i?d?w?k?a?)", result)
     ]
 
 
 def setacl(path, acl):
     """ Replaces ACL on specified path """
-    cfs = _which("cfs", "check your Coda client installation")
     _acl = [AclEntry.from_user(*entry) for entry in acl]
 
     positives = [(entry.name, entry.rights) for entry in _acl if entry.is_positive()]
@@ -100,8 +93,6 @@ def setacl(path, acl):
         (entry.name, entry.rights[1:]) for entry in _acl if entry.is_negative()
     ]
 
-    subprocess.call(list(itertools.chain([cfs, "sa", "-clear", str(path)], *positives)))
+    cfs("sa", "-clear", str(path), *itertools.chain(*positives))
     if negatives:
-        subprocess.call(
-            list(itertools.chain([cfs, "sa", "-negative", str(path)], *negatives))
-        )
+        cfs("sa", "-negative", str(path), *itertools.chain(*negatives))
